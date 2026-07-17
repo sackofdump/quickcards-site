d9c9a3d8cf8b1713741d7978b52be898735ff92d
Sackofdump <easlandjosh@gmail.com>
2026-07-17 08:01:21 -0500

    Auto-sync inventory: 6495 products (2026-07-17 13:01)

--- products.js
+++ products.js
@@ -15813,7 +15813,7 @@ window.products = [
     "badge": null,
     "discount": null,
     "numbered": false,
-    "stock": 1
+    "stock": 0
   },
   {
     "id": 1319,
@@ -41421,7 +41421,7 @@ window.products = [
     "badge": null,
     "discount": null,
     "numbered": false,
-    "stock": 1
+    "stock": 0
   },
   {
     "id": 3453,
